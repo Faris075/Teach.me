884947d87039dc2e8657efc433edb575a9c44e71
Faris Nabil <farisnabil075@gmail.com>
2026-05-23 05:10:41 +0300

    Auto-save: 2026-05-23 05:10:41

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role'       => \App\Http\Middleware\RoleMiddleware::class,
+            'multitenant' => \App\Http\Middleware\MultiTenantMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
