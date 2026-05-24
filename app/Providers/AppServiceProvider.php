f6662c21982302e468c696fc70e3bdb8fd5dae47
Faris Nabil <farisnabil075@gmail.com>
2026-05-24 06:55:01 +0300

    Auto-save: 2026-05-24 06:55:01

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Events\GradePublished;
 use App\Listeners\SendGradePublishedNotification;
+use App\View\Compilers\ReadableBladeCompiler;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,6 +17,17 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        // Use human-readable filenames in the Blade view cache
+        $this->app->extend('blade.compiler', function ($compiler, $app) {
+            return new ReadableBladeCompiler(
+                $app['files'],
+                $app['config']['view.compiled'],
+                $app->basePath(),
+                $app['config']['view.cache'] ?? true,
+                $app['config']['view.compiled_extension'] ?? 'php',
+            );
+        });
+
         Event::listen(GradePublished::class, SendGradePublishedNotification::class);
     }
 }
